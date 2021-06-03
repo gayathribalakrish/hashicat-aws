f152a15ff9ade8649d024b9ec71340a0020cd12f
gayathribalakrish <rbkgayathri@gmail.com>
2021-06-03 21:11:29 +0000

    updated s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "gayathri-bucket"
   acl    = "private"
-  bucket_prefix = "gayathri"
 
   versioning = {
     enabled = true
